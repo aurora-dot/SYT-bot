cf76092786c95a7f01fa6ab25f7aeb37b7fc2436
E <e@pastel.codes>
2022-05-26 17:59:34 +0100

    Remove unneeded methods

--- src/syt_bot/stream.py
+++ src/syt_bot/stream.py
@@ -1,4 +1,3 @@
-import discord
 from discord.ext import commands
 
 from syt_bot.youtube import YTDLSource
@@ -25,52 +24,23 @@ class Music(commands.Cog):
             await ctx.send("Not in a voice channel")
 
     @commands.command()
-    async def play(self, ctx, *, query):
-        """Plays a file from the local filesystem"""
-
-        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
-        ctx.voice_client.play(
-            source, after=lambda e: print(f"Player error:  { e } ") if e else None
-        )
-
-        await ctx.send(f"Now playing:  { query } ")
-
-    @commands.command()
-    async def yt(self, ctx, *, url):
-        """Plays from a url (almost anything youtube_dl supports)"""
-
-        async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop)
-            ctx.voice_client.play(
-                player, after=lambda e: print(f"Player error:  { e } ") if e else None
-            )
-
-        await ctx.send(f"Now playing:  { player . title } ")
-
-    @commands.command()
     async def stream(self, ctx, *, url):
-        """Streams from a url (same as yt, but doesn't predownload)"""
-
         async with ctx.typing():
             player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
             ctx.voice_client.play(
                 player, after=lambda e: print(f"Player error:  { e } ") if e else None
             )
 
-        await ctx.send(f"Now playing:  { player . title } ")
+        await ctx.send(f"Now playing:  { player.title } ")
 
     @commands.command()
     async def volume(self, ctx, volume: int):
-        """Changes the player's volume"""
-
         if ctx.voice_client is None:
             return await ctx.send("Not connected to a voice channel.")
 
         ctx.voice_client.source.volume = volume / 100
         await ctx.send(f"Changed volume to  { volume } %")
 
-    @play.before_invoke
-    @yt.before_invoke
     @stream.before_invoke
     async def ensure_voice(self, ctx):
         if ctx.voice_client is None:
